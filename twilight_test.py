be3672ec6f4552d9ef3da2b3b91c2d7224f2a6a3
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-04 01:54:36 -0700

    modify test script for new colors

--- twilight_test.py
+++ twilight_test.py
@@ -1,17 +1,18 @@
 from nose.tools import *
 import twilight
 
-def test_red_is_red():
-    assert twilight.sparkle.red == '\033[91m'
+def test_red():
+    assert twilight.sparkle.red == '\033[31m'
 
-def test_yellow_is_yellow():
+def test_yellow():
     assert twilight.sparkle.yellow == '\033[93m'
 
-def test_green_is_green():
-    assert twilight.sparkle.green == '\033[92m'
+def test_green():
+    assert twilight.sparkle.green == '\033[32m'
 
-def test_blue_is_blue():
-    assert twilight.sparkle.blue == '\033[94m'
+def test_blue():
+    assert twilight.sparkle.blue == '\033[34m'
 
-def test_reset_resets():
+def test_reset():
     assert twilight.sparkle.reset == '\033[0m'
+
