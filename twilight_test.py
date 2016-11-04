57b1c9722064f4ea3d8181436755f7466e47b505
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-03 23:08:00 -0700

    complete code coverage for twilight.sparkle

--- twilight_test.py
+++ twilight_test.py
@@ -3,3 +3,15 @@ import twilight
 
 def test_red_is_red():
     assert twilight.sparkle.red == '\033[91m'
+
+def test_yellow_is_yellow():
+    assert twilight.sparkle.yellow == '\033[93m'
+
+def test_green_is_green():
+    assert twilight.sparkle.green == '\033[92m'
+
+def test_blue_is_blue():
+    assert twilight.sparkle.blue == '\033[94m'
+
+def test_reset_resets():
+    assert twilight.sparkle.reset == '\033[0m'
